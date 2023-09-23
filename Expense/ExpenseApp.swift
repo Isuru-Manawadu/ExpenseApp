487aa75fa75c8adfecd957b952572f3009850bcb
Isuru-Manawadu <isurumanawadu1997@outlook.com>
2023-09-23 15:58:53 +0530

    Initial Commit

---
+++ Expense/ExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseApp.swift
+//  Expense
+//
+//  Created by Isuru Manawadu on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
