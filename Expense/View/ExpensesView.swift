85d5b781085d6731244bacd1d951934fda498c38
Isuru-Manawadu <isurumanawadu1997@outlook.com>
2023-09-27 00:57:20 +0530

    updated

--- Expense/View/ExpensesView.swift
+++ Expense/View/ExpensesView.swift
@@ -11,109 +11,154 @@ struct ExpensesView : View {
     
     @StateObject var ExpenseBV : ExpensesModel = ExpensesModel()
     
-        var body: some View {
-            ZStack{
-                Color.white.edgesIgnoringSafeArea(.all)
-                Color.white.opacity(0.04).ignoresSafeArea(edges : .top)
+    //    @State private var selectedCategory = 0
+//    @State private var selectedCategory: String = ""
+//    let categories = ["Health, transport, Foods"]
+    
+    
+    var body: some View {
+        ZStack{
+            Color.white.edgesIgnoringSafeArea(.all)
+            Color.white.opacity(0.04).ignoresSafeArea(edges : .top)
+            
+            VStack{
+                LinearGradient(colors: [Color(""), Color("")],
+                               startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea(edges : .top)
+                    .frame(height: 200)
+                    .overlay {
+                        VStack {
+                            Text("Your Expenses").font(.system(size: 30))
+                                .bold()
+                                .foregroundColor(.black)
+                            
+                        }
+                    }
                 
-                VStack{
-                    LinearGradient(colors: [Color(""), Color("")],
-                                   startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea(edges : .top)
-                        .frame(height: 200)
-                        .overlay {
-                            VStack {
-                                Text("Your Expenses").font(.system(size: 30))
-                                    .bold()
-                                    .foregroundColor(.black)
-                                
+                
+                VStack(spacing : 20){
+                    
+                    
+                    VStack{
+                        RoundedRectangle(cornerRadius: 70)
+                            .foregroundColor(.white)
+                            .frame(height: 50)
+                            .overlay {
+                                TextField("Amount", text: $ExpenseBV.amount1)
+                                    .padding(.leading, 10)
                             }
-                        }
+                    }
+                    .padding(.horizontal , 20)
+                    
+                    VStack{
+                        RoundedRectangle(cornerRadius: 70)
+                            .foregroundColor(.white)
+                            .frame(height: 50)
+                            .overlay {
+                                SecureField("Description", text: $ExpenseBV.description1)
+                                    .padding(.leading, 10)
+                            }
+                    }
+                    .padding(.horizontal , 20)
                     
                     
-                    VStack(spacing : 20){
-                        
-                        
-                        VStack{
-                            RoundedRectangle(cornerRadius: 70)
-                                .foregroundColor(.white)
-                                .frame(height: 50)
-                                .overlay {
-                                    TextField("Amount", text: $ExpenseBV.amount1)
-                                        .padding(.leading, 10)
-                                }
-                        }
-                        .padding(.horizontal , 20)
-                                                
-                        VStack{
-                            RoundedRectangle(cornerRadius: 70)
-                                .foregroundColor(.white)
-                                .frame(height: 50)
-                                .overlay {
-                                    SecureField("Description", text: $ExpenseBV.description1)
-                                        .padding(.leading, 10)
-                                }
-                        }
-                        .padding(.horizontal , 20)
-                        
-                        VStack{
-                            RoundedRectangle(cornerRadius: 70)
-                                .foregroundColor(.white)
-                                .frame(height: 50)
-                                .overlay {
-                                    SecureField("Category", text: $ExpenseBV.category1)
-                                        .padding(.leading, 10)
-                                }
-                        }
-                        .padding(.horizontal , 20)
-                        
-                        VStack{
-                            RoundedRectangle(cornerRadius: 70)
-                                .foregroundColor(.white)
-                                .frame(height: 50)
-                                .overlay {
-                                    SecureField("Date", text: $ExpenseBV.date1)
-                                        .padding(.leading, 10)
-                                }
-                        }
-                        .padding(.horizontal , 20)
-                     
-                        Button {
-                            
-                        } label: {
-                            
-                            ZStack {
-                                LinearGradient(colors: [Color("Blue"), Color("Blue")], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea(edges : .top).clipShape(RoundedRectangle(cornerRadius: 70))
-                                    .frame(height: 50)
-                                Text("Add Your Expenses")
-                                    .foregroundColor(.white)
-                            }.padding(.horizontal , 20)
-                            
-                        }
-                        
-                        Button {
-                            
-                        } label: {
-                            
-                            ZStack {
-                                LinearGradient(colors: [Color("Gradient 2"), Color("Gradient 2")], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea(edges : .top).clipShape(RoundedRectangle(cornerRadius: 70))
-                                    .frame(height: 50)
-                                Text("Clear Fields")
-                                    .foregroundColor(.white)
-                            }.padding(.horizontal , 20)
-                            
-                        }
-                        
-                    }.padding()
                     
-                        .offset(y: -50)
-                    
-                    
-                    Spacer()
+                    Menu{
+                        Button(role: .destructive){
+                            print("")
+                        }label: {
+                            Text("Health")
+                        }
+                        Button("transport"){
+                            print("")
+                        }
+                        Button("Foods"){
+                            print("")
+                        }
+                        Button("Security"){
+                            print("")
+                        }
+                    } label: {
+                        Text("Category")
+                    } primaryAction: {
+                        print("")
+                    }
                 }
+                    
+                    
+//                    VStack{
+//                        RoundedRectangle(cornerRadius: 70)
+//                            .foregroundColor(.white)
+//                            .frame(height: 50)
+//                            .overlay {
+//                                SecureField("Category", text: $ExpenseBV.category1)
+//                                    .padding(.leading, 10)
+//                            }
+//                    }
+//                    .padding(.horizontal , 20)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    VStack{
+                        RoundedRectangle(cornerRadius: 70)
+                            .foregroundColor(.white)
+                            .frame(height: 50)
+                            .overlay {
+                                SecureField("Date", text: $ExpenseBV.date1)
+                                    .padding(.leading, 10)
+                            }
+                    }
+                    .padding(.horizontal , 20)
+                    
+                    Button {
+                        
+                    } label: {
+                        
+                        ZStack {
+                            LinearGradient(colors: [Color("Blue"), Color("Blue")], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea(edges : .top).clipShape(RoundedRectangle(cornerRadius: 70))
+                                .frame(height: 50)
+                            Text("Add Your Expenses")
+                                .foregroundColor(.white)
+                        }.padding(.horizontal , 20)
+                        
+                    }
+                    
+                    Button {
+                        
+                    } label: {
+                        
+                        ZStack {
+                            LinearGradient(colors: [Color("Gradient 2"), Color("Gradient 2")], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea(edges : .top).clipShape(RoundedRectangle(cornerRadius: 70))
+                                .frame(height: 50)
+                            Text("Clear Fields")
+                                .foregroundColor(.white)
+                        }.padding(.horizontal , 20)
+                        
+                    }
+                    
+                }.padding()
+                
+                    .offset(y: -50)
                 
                 
-                
+                Spacer()
             }
+            
+            
+            
         }
     }
 
